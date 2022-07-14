bb02bf5044ff0a1151e478c2465404b47624a71f
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2022-07-14 14:13:42 +0200

    Generic Filter with predicate callback

--- main.go
+++ main.go
@@ -18,8 +18,24 @@ func main() {
 	structResults = addStudent(structResults, student)
 	fmt.Println(results)
 	fmt.Println(structResults)
+
+	floats := []float64{14.1, 15.7, 16.3, 15.8, 83.2}
+	filtered := GenFilter(floats, func(item float64) bool {
+		return item < 16
+	})
+	fmt.Println(filtered)
 }
 
 func addStudent[T any](students []T, newStudent T) []T {
 	return append(students, newStudent)
+}
+
+func GenFilter[T any](input []T, f func(T) bool) []T {
+	results := make([]T, 0)
+	for _, item := range input {
+		if f(item) {
+			results = append(results, item)
+		}
+	}
+	return results
 }
