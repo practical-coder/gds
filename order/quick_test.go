666ec823316e000f75ebf9fdb9b5b44c8b5cb050
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2023-01-06 15:54:55 +0100

    Sorting: quicksort - testing

--- order/quick_test.go
+++ order/quick_test.go
@@ -1,7 +1,30 @@
 package order
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestQuicksort(t *testing.T) {
-	
+	names := []string{
+		"Zuza",
+		"Asia",
+		"Basia",
+		"Kasia",
+		"Magda",
+		"Iza",
+	}
+	Quicksort[string](names, 0, len(names)-1)
+	expected := []string{
+		"Asia",
+		"Basia",
+		"Iza",
+		"Kasia",
+		"Magda",
+		"Zuza",
+	}
+	if !cmp.Equal(names, expected) {
+		t.Errorf("names: %v\nexpected: %v\n", names, expected)
+	}
 }
