6e1584752e1c797734271791cbccb35495a95983
Jacek Witkiewicz <jacek.witkiewicz@gmail.com>
2023-01-08 12:44:05 +0100

    comments out

--- staff/employee_test.go
+++ staff/employee_test.go
@@ -6,13 +6,13 @@ import (
 )
 
 func TestEmployee(t *testing.T) {
-	person := new(employee) // Returns the address of an employee
+	person := new(employee)
 	person.SetFirstName("Helen")
 	person.SetLastName("Rose")
 	person.SetRole("Technical Lead")
 	person.SetSalary(125_644.0)
 	fmt.Println(person)
-	hourlyWorker := new(partTimeEmployee) // Returns address
+	hourlyWorker := new(partTimeEmployee)
 	hourlyWorker.SetFirstName("Mark")
 	hourlyWorker.SetLastName("Smith")
 	hourlyWorker.SetRole("Software Developer")
